0e39d72b4da27f16d032cfbcab8b03243b809e42
jared-two-foxes <jared-two-foxes@users.noreply.github.com>
2024-03-13 15:57:34 +1300

    start building the server main loop in server test

--- tests/server.rs
+++ tests/server.rs
@@ -5,15 +5,20 @@ use blackjack::{Action, DataSource};
 
 // import our lib and setup a game
 #[test]
-fn can_setup_games() {
+fn can_play_a_simple_game() {
     let mut ds = DataSource::default();
     let game_id = blackjack::add_game(&mut ds);
+    //@todo: swap in a loaded deck
     let hand_id = blackjack::add_player(&mut ds, game_id);
 
     blackjack::add_action(&mut ds, hand_id, Action::Hit);
-    let _ = blackjack::process_actions(&ds.actions, &mut ds.hands, &mut ds.decks); //< this could
-                                                                                   //error so we should do something about that.
+    let hands = blackjack::process_actions(&ds.actions, &mut ds.hands, &mut ds.decks)?; //< this could error so we should do something about that?
 
-    assert!(false); // Need to figure out exactly what we are asserting on.
+    hands = blackjack::update_hand_states(&hands);
+    
+    //@todo: merge the modified hands back in to the list.
+  
+    assert_eq!(blackjack::Outcome::Won, get_game_outcome(&ds. 
+    hands, hand_id));
 }
 
