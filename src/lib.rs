160e35b1be1087373b8c754c6e1ae259f1f048d6
Jared Watt <jared@twofoxes.co.nz>
2024-04-26 22:25:59 +1200

    Updating the base core loop functionality

--- src/lib.rs
+++ src/lib.rs
@@ -21,18 +21,17 @@ pub type Card = (Suit, Value);
 pub struct Hand {
     pub id: Uuid,
     player: Uuid,
-    pub game: Uuid,
-    dealer: bool,
+    dealer: Uuid,
 }
 
-fn hand_value(cards: &Vec<&Card>) -> u8 {
+fn hand_value(cards: &[&Card]) -> u8 {
     let mut ace_count = 0;
     let mut value = cards
         .iter()
         .map(|c| match c.1 {
             Value::Value(v) => v,
             Value::Ace => {
-                ace_count = ace_count + 1;
+                ace_count += 1;
                 11
             }
             _ => 10,
@@ -40,14 +39,10 @@ fn hand_value(cards: &Vec<&Card>) -> u8 {
         .sum::<u8>();
     for _ in 0..ace_count {
         if value > 21 {
-            value = value - 10;
+            value -= 10;
         }
     }
     value
-}
-
-fn hand_bust(cards: &Vec<&Card>) -> bool {
-    hand_value(cards) > 21
 }
 
 pub type Deck = Vec<Card>;
@@ -118,24 +113,27 @@ pub struct CardAllocation {
     pub card_idx: usize,
 }
 
-//@todo: i think its a good idea to split this out since the outcome of these actions is different which is making it hard to build out the functions to process as the return types get hard
+// @todo: I've seen this Hold referenced as "Stand" which I guess makes more sense?
 pub enum Action {
     Hit,
     Hold,
 }
 
-//< @todo:  We should store the hand value here.
 pub enum State {
     Active,
     Holding(u8),
     Bust,
+    BlackJack,
 }
-
-// Pair mapping hand to its current state.
-pub type HandState = (Uuid, State);
 
 //pair mapping hand to an action
 pub type HandAction = (Uuid, Action);
+
+// Pair mapping hand to its current state.
+// @todo: This is internal only, maybe we should replace this dealer uuid with a direct index back
+// into the array to avoid the O(n) finds required to get the dealers HandState when calculating
+// the HandOutcome.
+pub type HandState = (Uuid /*this*/, Uuid /*dealer*/, State);
 
 #[derive(Default)]
 pub struct DataSource {
@@ -144,6 +142,7 @@ pub struct DataSource {
     pub allocations: Vec<CardAllocation>,
     pub hand_states: Vec<HandState>,
     pub actions: Vec<HandAction>,
+    pub outcomes: Vec<HandOutcome>,
 }
 
 //@thoughts: Should something that takes a mut ref be part of the impl block of
@@ -152,24 +151,25 @@ pub struct DataSource {
 //           the DataSource?
 
 pub fn add_game(ds: &mut DataSource) -> Uuid {
-    let game_id = Uuid::default();
-    ds.decks.insert(game_id, new_deck());
+    let dealer_id = Uuid::new_v4();
+    ds.decks.insert(dealer_id, new_deck());
     ds.hands.push(Hand {
-        id: Uuid::default(),
-        player: Uuid::default(),
-        game: game_id,
-        dealer: true,
+        id: dealer_id,
+        player: dealer_id,
+        dealer: Uuid::nil(),
     });
-    game_id
+    dealer_id
 }
 
-pub fn add_player(ds: &mut DataSource, game_id: Uuid) -> Uuid {
-    let player_id = Uuid::default();
+//@todo: this is a little awkward.  We should potentially have a second function to
+// create a hand which returns the hand_id else how does the client know how to add
+// an action?
+pub fn add_player(ds: &mut DataSource, dealer_id: Uuid) -> Uuid {
+    let player_id = Uuid::new_v4();
     ds.hands.push(Hand {
-        id: Uuid::default(),
+        id: player_id,
         player: player_id,
-        game: game_id,
-        dealer: false,
+        dealer: dealer_id,
     });
 
     player_id
@@ -181,10 +181,6 @@ pub fn add_player(ds: &mut DataSource, game_id: Uuid) -> Uuid {
 pub fn add_action(ds: &mut DataSource, hand_id: Uuid, action: Action) {
     ds.actions.push((hand_id, action));
 }
-
-//
-// Systems/Controllers
-//
 
 pub enum ActionResolutionError {
     MissingHand,
@@ -198,18 +194,20 @@ pub enum ActionResolutionError {
 //       in this list that are going to end up hitting the same deck and therefore
 //       invalidating the number deck index calculated from the allocations list.
 pub fn process_hit_actions(
-    actions: &Vec<HandAction>,
+    actions: &[HandAction],
     allocations: &[CardAllocation],
 ) -> Vec<CardAllocation> {
-    actions.iter().filter(|(_,action) matches!(Action::Hit, action)).map(|(hand,_)|
-    {
-                let card_idx = allocations.iter().filter(|a| a.hand == *hand).count();
-                new_allocations.push(CardAllocation {
-                    card_idx,
-                    hand: *hand,
-                });
+    actions
+        .iter()
+        .filter(|(_, action)| matches!(Action::Hit, action))
+        .map(|(hand, _)| {
+            let card_idx = allocations.iter().filter(|a| a.hand == *hand).count();
+            CardAllocation {
+                card_idx,
+                hand: *hand,
             }
-    }).collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>()
 }
 
 pub fn process_hand_states(
@@ -219,8 +217,10 @@ pub fn process_hand_states(
 ) -> Vec<HandState> {
     let mut hand_states = Vec::new();
     for h in hands {
-        let deck = decks.get(&h.game).expect("Unable to find deck for table");
-        //@note: its probably faster to just build the hand values by iterating this once and building it as we go foldish style and then map that into a hand_state rather than iterate all the allocations for each hand like this.
+        let deck = decks.get(&h.dealer).expect("Unable to find deck for table");
+        //@note: its probably faster to just build the hand values by iterating this once and building
+        //  it as we go foldish style and then map that into a hand_state rather than iterate all the
+        //  allocations for each hand like this.
         let cards = card_allocations
             .iter()
             .filter(|a| a.hand == h.id)
@@ -228,83 +228,56 @@ pub fn process_hand_states(
             .collect::<Vec<_>>();
 
         //@note: its probably better to just not add the actives here rather than strip them out later.
-        let state = match  hand_value(&cards) {
+        let state = match hand_value(&cards) {
             0..=20 => State::Active,
             21 => State::BlackJack,
             _ => State::Bust,
         };
-        hand_states.push((h.id, state));
+        hand_states.push((h.id, h.dealer, state));
     }
-    
     // and strip out all the Active's because we dont want to report those.
-    
-    hand_states.iter().filter(|hs| not matches!(hs, State::Active)).collect()
+    hand_states
+        .into_iter()
+        .filter(|(_, _, hs)| !matches!(hs, State::Active))
+        .collect()
 }
 
 pub fn process_hold_actions(
-    actions: &Vec<HandAction>,
+    hands: &[Hand],
+    actions: &[HandAction],
     allocations: &[CardAllocation],
-    decks: &HashMap<Uuid, Deck>
+    decks: &HashMap<Uuid, Deck>,
 ) -> Vec<HandState> {
-    actions.iter().filter(|(_,action) matches!(Action::Hold, action)).map(|(hand,_)| {
-    let deck = decks.get(&hand.game).expect("Unable to find deck for table");
-    let cards = card_allocations
-            .iter()
-            .filter(|a| a.hand == h.id)
-            .map(|a| &deck[a.card_idx])
-            .collect::<Vec<_>>();
-    HandState(hand, State::Holding(hand_value(cards)) 
-    }).collect::<Vec<_>>()
+    actions
+        .iter()
+        .filter(|(_, action)| matches!(Action::Hold, action))
+        .map(|(hand, _)| {
+            // Grab the dealers uuid.
+            let dealer_id = hands
+                .iter()
+                .find(|h| h.id == *hand)
+                .expect("Unable to find the hand of a given hand_action")
+                .dealer;
+
+            // Grab the deck for the hand
+            let deck = decks
+                .get(&dealer_id)
+                .expect("Unable to find deck for table");
+
+            // Calculate the hand value
+            let cards = allocations
+                .iter()
+                .filter(|a| a.hand == *hand)
+                .map(|a| &deck[a.card_idx])
+                .collect::<Vec<_>>();
+            let value = hand_value(&cards);
+
+            (*hand, dealer_id, State::Holding(value))
+        })
+        .collect::<Vec<_>>()
 }
 
-/*pub fn resolve_table_state(
-    decks: &HashMap<Uuid, Deck>,
-    hands: &[Hand],
-    allocations: &[CardAllocation],
-) -> Vec<HandState> {
-    let mut hand_states = Vec::new();
-
-    //@note: We have one deck per table so iterating over the keys is a way to
-    //       get all of the table identifiers but we are also going to want the
-    //       decks so we're just going to iterate over the decks list.
-    decks.iter().for_each(|(&game_id, deck)| {
-        // Retrieve all of the hands being played on this table.
-        let game_hands = hands
-            .iter()
-            .filter(|h| h.game == game_id)
-            .collect::<Vec<_>>();
-
-        // Grab all the cards for the hands at the table.
-        let hand_cards = game_hands
-            .iter()
-            .map(|h| {
-                (
-                    h.id,
-                    allocations
-                        .iter()
-                        .filter(|a| a.hand == h.id)
-                        .map(|a| &deck[a.card_idx])
-                        .collect::<Vec<_>>(),
-                )
-            })
-            .collect::<Vec<_>>();
-
-        let hand_values = hand_cards
-            .iter()
-            .map(|(id, cards)| {
-                let value = hand_value(cards);
-                let state = if value > 21 {
-                    State::Bust
-                } else {
-                    State::Active
-                };
-                (id, value, state)
-            })
-            .collect::<Vec<_>>();
-    });
-    hand_states
-}*/
-
+#[derive(Debug, Clone, PartialEq)]
 pub enum Outcome {
     Won,
     Lost,
@@ -312,42 +285,40 @@ pub enum Outcome {
 
 pub type HandOutcome = (Uuid, Outcome);
 
-//
-pub fn resolve_outcomes(hands: &Vec<Hand>, hand_values: &Vec<(Uuid, u8)>, outcomes: Vec<Outcome>) -> Vec<HandOutcome> {
-    let idx:usize = 0;
-    let outcome_size = outcomes.len();
+// Iterate all of the HandStates in hand_state, for any HandState for which there is a corosponding
+// dealer HandState determine the HandOutcome and return it.
+pub fn resolve_outcomes(hand_values: &[HandState], outcomes: &[HandOutcome]) -> Vec<HandOutcome> {
+    hand_values
+        .iter()
+        // Check if this particular hand already exists within the outcomes list
+        .filter(|h| outcomes.iter().any(|o| o.0 == h.0))
+        // Grab the dealer value and if it exist return (hand, dealer) pair,
+        // filter out this hand if the dealer state does not exist.
+        .filter_map(|h| hand_values.iter().find(|hv| hv.0 == h.1).map(|d| (h, d)))
+        // And finally lets determine the outcome.
+        .map(|(h, d)| {
+            let state = if h.1 > d.1 {
+                Outcome::Won
+            } else {
+                Outcome::Lost
+            };
+            (h.0, state)
+        })
+        .collect::<_>()
+}
 
-        hand_values
-            .iter()
-            .filter(|(value_id,_)| { 
-                if idx > outcomes_size { return true; }
-                let (outcome_id,_) = outcomes[idx];
-                if (value_id == outcome_id) {
-                    idx += 1;
-                } 
-                value_id != outcome_id 
-            })
-            .filter_map(|(id, dealer,_)| {
-                let dealer_hand_value = hand_values.find(|(id,_,_)| dealer == id)
-                match dealer_hand_value {
-                    Some(dealer_hand) => Some(id,dealer_hand, value)
-                    _ => None
-                }
-                
-            })
-            .map(|(id, dealer, value)| {
-                let state = if value > dealer_hand_value {
-                    Outcome::Won
-                } else {
-                    Outcome::Lost
-                };
-                (id, state)
-            })
-            .collect::<Vec<_>>()
+pub fn is_game_complete(dealer: Uuid, hands: &[Hand], outcomes: &[HandOutcome]) -> bool {
+    unimplemented!("for a given hand, a game is complete when all of the hands assocaited with the same dealer have an Outcome")
+}
+
+// @todo:  I guess we need to keep this "clone" but I dont like it.
+pub fn get_hand_outcome(hand_id: Uuid, outcomes: &[HandOutcome]) -> Option<Outcome> {
+    outcomes
+        .iter()
+        .find(|o| o.0 == hand_id)
+        .map(|o| o.1.clone())
 }
 
 // turn sequence; the order in which players take turns (with the dealer going last)
-
 // action validation; an action is only balid if its that players turn to go.
-
 // Rules engine?
--- tests/server.rs
+++ tests/server.rs
@@ -13,38 +13,47 @@ fn can_play_a_simple_game() {
 
     blackjack::add_action(&mut ds, hand_id, Action::Hit);
 
-    // Building out a single "round", where a round involves reacting to a 
+    // Building out a single "round", where a round involves reacting to a
     // collection of hand actions and then detrrmining the results for each
     // hand that was acted upon
     {
         // allocate new cards from hit actions
-        let allocations = blackjack::process_hit_actions(&ds.actions, &ds.allocations); 
+        let allocations = blackjack::process_hit_actions(&ds.actions, &ds.allocations);
+
         // Determine which hands have had cards allocated to then.
-        let hands = allocations.iter().map(|a| a.hand).collect::<Vec<_>>();
-        
+        /*let hands = allocations
+        .iter()
+        .filter_map(|a| ds.hands.iter().find(|h| h.id == a.hand))
+        .collect::<Vec<_>>();*/
+
         // Merge allocations into the master list.
         ds.allocations.extend(allocations);
-        
-        
-        // Check if any of the hands have busted.
-        let resulting_states = blackjack::resolve_hand_states(&hands, &ds.allocations, &ds.decks);
-        
-        todo!("need to add a step here to iterate hand states to check for children that need to be added?");
-        
+
+        // Check for updates to the hand states.
+        let resulting_states =
+            blackjack::process_hand_states(/*&hands*/ &ds.hands, &ds.allocations, &ds.decks);
+
+        //todo!("need to add a step here to iterate hand states to check for children that need to be added");
+
         // Merge into the master state list
         ds.hand_states.extend(resulting_states);
 
         // process all the hold actions.
-        let hold_states = blackjack::process_hold_action(&ds.actions, &ds.allocations);
-        
+        let hold_states =
+            blackjack::process_hold_actions(&ds.hands, &ds.actions, &ds.allocations, &ds.decks);
+
         // Merge these into the master state list
         ds.hand_states.extend(hold_states);
-    
+    }
+
     //assert!(that the game is over)
 
     // determine the outcome of the game.
-    blackjack::resolve_outcomes(&ds.hands, &ds.hand_states);
-    }
+    let outcomes = blackjack::resolve_outcomes(&ds.hand_states, &ds.outcomes);
+    ds.outcomes.extend(outcomes);
 
-    assert_eq!(blackjack::Outcome::Won, get_hand_outcome(hand_id, &ds.hands));
+    assert_eq!(
+        Some(blackjack::Outcome::Won),
+        blackjack::get_hand_outcome(hand_id, &ds.outcomes)
+    );
 }
